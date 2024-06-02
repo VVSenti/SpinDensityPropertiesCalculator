964e06768bb49f1d82b714977b1bb3add95af72a
VVSenti <47742065+VVSenti@users.noreply.github.com>
2024-06-02 22:58:42 +0300

    m

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,9 @@
-import javax.swing.SwingUtilities;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class Main {
-		
+
 	public static void main(String[] args) {
-//		SwingUtilities.invokeLater(() -> new Main());
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-		
 
 	}
 }
