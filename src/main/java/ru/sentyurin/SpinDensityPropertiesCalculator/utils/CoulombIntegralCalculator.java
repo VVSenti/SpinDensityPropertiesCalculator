322d98b5a5156bd51c21859989d434a44ead4160
VVSenti <sentyurinvv@gmail.com>
2024-10-08 16:08:42 +0300

    Refactoring with ExecutorService

--- src/main/java/ru/sentyurin/SpinDensityPropertiesCalculator/utils/CoulombIntegralCalculator.java
+++ src/main/java/ru/sentyurin/SpinDensityPropertiesCalculator/utils/CoulombIntegralCalculator.java
@@ -1,7 +1,13 @@
 package ru.sentyurin.SpinDensityPropertiesCalculator.utils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.DoubleAdder;
 
@@ -18,18 +24,19 @@ public class CoulombIntegralCalculator implements CalculatorInterruptable {
 		return "Coulomb integral";
 	}
 
-	private double getCoulombIntegralInterruptable(List<double[]> data, double diffVolume) throws InterruptedException {
+	private double getCoulombIntegralInterruptable(List<double[]> data, double diffVolume)
+			throws InterruptedException {
 		AtomicInteger startPointIndex = new AtomicInteger();
-		int maxThreads = Runtime.getRuntime().availableProcessors() - 1;
-		int pointCount = data.size();
-		DoubleAdder integral = new DoubleAdder();
+		final int maxThreads = Runtime.getRuntime().availableProcessors() - 1;
+		final int pointCount = data.size();
 
-		Thread[] threads = new Thread[maxThreads];
+		ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
+		List<Future<Double>> temporalResults = new ArrayList<Future<Double>>(maxThreads);
 
 		for (int i = 0; i < maxThreads; i++) {
-			Runnable task = new Runnable() {
-				@Override
-				public void run() {
+			Callable<Double> task = new Callable<Double>() {
+				public Double call() {
+					double integral = 0.0;
 					boolean stop = false;
 					while (!stop) {
 						int startingIndex = startPointIndex.getAndIncrement();
@@ -40,36 +47,39 @@ public class CoulombIntegralCalculator implements CalculatorInterruptable {
 						for (int m = startingIndex + 1; m < pointCount; m++) {
 							double[] secondPoint = data.get(m);
 							double distanceSquare = Math.pow(firstPoint[0] - secondPoint[0], 2);
-							distanceSquare += (firstPoint[1] - secondPoint[1]) * (firstPoint[1] - secondPoint[1]);
-							distanceSquare += (firstPoint[2] - secondPoint[2]) * (firstPoint[2] - secondPoint[2]);
+							distanceSquare += (firstPoint[1] - secondPoint[1])
+									* (firstPoint[1] - secondPoint[1]);
+							distanceSquare += (firstPoint[2] - secondPoint[2])
+									* (firstPoint[2] - secondPoint[2]);
 							temp += secondPoint[3] / Math.sqrt(distanceSquare);
 						}
-						integral.add(firstPoint[3] * temp);
+						integral += firstPoint[3] * temp;
 						stop = toInterrupt;
 					}
+					return integral;
 				}
 			};
-			Thread thread = new Thread(task);
-			thread.start();
-			threads[i] = thread;
+			temporalResults.add(executor.submit(task));
 		}
 
-		for (Thread x : threads) {
+		Double integral = 0.0;
+		for (Future<Double> futureValue : temporalResults) {
 			try {
-				x.join();
-			} catch (InterruptedException e) {
+				integral += futureValue.get();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		return integral.doubleValue() * diffVolume * diffVolume * 627.5095 * 2.0 / 1.8897259886;
+
+		return integral * diffVolume * diffVolume * 627.5095 * 2.0 / 1.8897259886;
 	}
 
 	/**
 	 * Вычисляет значение интеграла спиновой плотности.
 	 */
 	@Override
-	public double calculate(SquareGridData squareGridData) throws IOException, InterruptedException {
+	public double calculate(SquareGridData squareGridData)
+			throws IOException, InterruptedException {
 		double diffVolume = squareGridData.getDiffVolume();
 		toInterrupt = false;
 		return getCoulombIntegralInterruptable(squareGridData.getData(), diffVolume);
